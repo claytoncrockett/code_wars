16c21ca65c62406ac5b836cecf030e877798e74b
Clayton Crockett <claytoncrockett@gmail.com>
2018-10-30 18:23:35 -0600

    rgb to hex

---
+++ rgbToHex.js
@@ -0,0 +1,24 @@
+function rgb(r, g, b){
+  var solution = setValues(r, g, b)
+  return solution
+}
+
+function setValues(r, g, b){
+  var a = setValue(r);
+  var q = setValue(g);
+  var c = setValue(b);
+  var hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+  return (hex[a[0]] + hex[a[1]] + hex[q[0]] + hex[q[1]] + hex[c[0]] + hex[c[1]]);
+}
+
+function setValue(x){
+  if (x < 0) return [0, 0];
+  if (x > 255) return [15, 15];
+  arr = [0, 0];
+  while (x > 15){
+    arr[0]++;
+    x -= 16;
+  }
+  arr[1] = x;
+  return arr;
+}
